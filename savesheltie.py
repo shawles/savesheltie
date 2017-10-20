db27066d6ced2d12f50de0c1516c22d24cc2253f
shawles <shalimova-ekaterina@yandex.ru>
2017-10-20 22:33:31 +0300

    New API and access_token saver

--- savesheltie.py
+++ savesheltie.py
@@ -22,14 +22,18 @@ def get_str_likes(x):
 	else:
 		return str(x)
 
-IMG_SIZE = 600
-NUM_POSTS = 600
+IMG_SIZE = 512
+NUM_POSTS = 10
 
 
 DOMAIN = "pusheltie"
-URL = "https://api.vk.com/method/wall.get?domain=%s&offset=%%s&count=1&v=5.45" % DOMAIN
+URL = "https://api.vk.com/method/wall.get?domain=" + DOMAIN + "&access_token=831749168317491683174916108348413b8831783174916dafd0a5f8bc8c5746d323a43" + "&offset=";
+ 
 GROUP_URL = "https://api.vk.com/method/groups.getById?group_id=%s" % DOMAIN
 PUBLIC_ID = get_response(GROUP_URL)["response"][0]["gid"]
+
+print(PUBLIC_ID)
+print(URL)
 
 POST_URL = "https://vk.com/wall-%s_%%s" % PUBLIC_ID
 
@@ -38,39 +42,41 @@ POST_URL = "https://vk.com/wall-%s_%%s" % PUBLIC_ID
 
 #img_num = len(get_response(URL % 0)["response"]["items"][0]["attachments"])
 
-PATH = "/home/shali_new/pushelties/"
+PATH = "/home/shali/pushelties/"
 
-alr = open(PATH + "gt.txt", 'r').readlines()
+alr = ""
+#alr = open(PATH + "gt.txt", 'r').readlines()
 #alr.close()
 
 data = open(PATH + "gt.txt", 'a')
 
 
-for p in range(300, NUM_POSTS):
-	get_resp = get_response(URL % p)
-	likes = get_resp["response"]["items"][0]["likes"]["count"]
-	if ((likes) < 190):
-		#print("\n\n")
-		#print(get_resp["response"]["items"][0])
-		if ("copy_history" not in get_resp["response"]["items"][0]):
-			img_num = len(get_resp["response"]["items"][0]["attachments"])
-			for i in range(img_num):
-				if ("photo" in get_resp["response"]["items"][0]["attachments"][i]):
-					#print(get_resp["response"]["items"][0]["attachments"][i]["photo"])
-					img_url = get_resp["response"]["items"][0]["attachments"][i]["photo"]["photo_604"]
-					img_addr = img_url[len(img_url) - 15:]
-					imgname = PATH + get_str_likes(likes) + img_addr
-					print(p, img_url[len(img_url) - 3:])
-					if ((img_url[len(img_url) - 3:] != 'gif') and (imgname not in alr)):
-						print(imgname)
-						data.write(imgname)
-						data.write("\n")
-						urllib.request.urlretrieve(img_url, imgname)
-						#img = imread(imgname)
-						#img = resize(img, (IMG_SIZE, IMG_SIZE, 3))
-						#imsave(imgname, img)
-					
-					#print("\n\n\n")
+for p in range(2, NUM_POSTS):
+	get_resp = get_response(URL + str(p) + "&count=1")
+	print(get_resp)
+	print('\n\n')
+	#прикреплено:
+	#print(get_resp["response"][1]["attachments"])
+	atts = get_resp["response"][1]["attachments"]
+	print('ЭЭЭЭ', atts, 'ЭЭЭЭ')
+	att_num = len(atts)
+	for i in range(att_num):
+		if ("photo" in atts[i]):
+			#print(get_resp["response"]["items"][0]["attachments"][i]["photo"])
+			img_url = atts[i]["photo"]["src_big"]
+			img_name = PATH + img_url[len(img_url) - 15:]
+			#imgname = PATH + get_str_likes(likes) + img_addr
+			#print(p, img_url[len(img_url) - 3:])
+			#if ((img_url[len(img_url) - 3:] != 'gif') and (imgname not in alr)):
+			#print(imgname)
+			data.write(img_name)
+			data.write("\n")
+			urllib.request.urlretrieve(img_url, img_name)
+			#img = imread(imgname)
+			#img = resize(img, (IMG_SIZE, IMG_SIZE, 3))
+			#imsave(imgname, img)
+			
+			#print("\n\n\n")
 
 #print("LIKES", get_response(URL % 0)["response"][0]["likes"])
 #imsave("shelt.jpg", img)
